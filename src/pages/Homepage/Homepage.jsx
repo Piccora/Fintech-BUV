209a72502900a89635c4f498bb4f54c86feea055
98O7 <lethehien2911@gmail.com>
2024-06-09 00:53:41 +0700

    homepage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -6,7 +6,7 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
-
+import './homepage.css'
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -20,10 +20,10 @@ export default function Homepage() {
     return (
         <>
         <HomePageCarousel />
-        <Stack spacing={10} sx={{ pt: 10 }}>
+        <Stack spacing={10} sx={{ pt: 10 }} className='header'>
             <Item>
                 <Typography component="div">
-                    <Box>
+                    <Box className="body1">
                         <Typography sx={{ fontWeight: 'bold', m: 1 }} variant="h3">About us</Typography>
                         <Typography sx={{ pt: 5, mx: 10 }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
                     </Box>
@@ -31,28 +31,28 @@ export default function Homepage() {
             </Item>
             <Item>
                 <Typography component="div">
-                    <Box>
+                    <Box className="body2">
                         <Typography sx={{ fontWeight: 'bold', m: 1 }} variant="h3">Our product</Typography>
                         <Typography sx={{ pt: 5, mx: 10 }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
                     </Box>
                 </Typography>
             </Item>
             <Item>
-                <Typography component="div">
-                    <Box>
+                <Typography component="div" className=''>
+                    <Box className="body3">
                         <Typography sx={{ fontWeight: 'bold', m: 1 }} variant="h3">Our team</Typography>
                         <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-                        <OurTeamCard />
-                        <OurTeamCard />
-                        <OurTeamCard />
-                        <OurTeamCard />
+                        <OurTeamCard className='member1' />
+                        <OurTeamCard className='member2'/>
+                        <OurTeamCard className='member3'/>
+                        <OurTeamCard className='member4'/>
                         </Box>
                     </Box>
                 </Typography>
             </Item>
             <Item>
             <Typography component="div">
-                    <Box>
+                    <Box className="body4">
                         <Typography sx={{ fontWeight: 'bold', m: 1 }} variant="h3">Our vision</Typography>
                         <Typography sx={{ pt: 5, mx: 10 }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
                     </Box>
